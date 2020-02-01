b80776171cc789ff2a4c6faa63059ad71d5bc6e0
arthurdobrea <arthurdobrya@gmail.com>
2020-02-01 22:37:44 +0200

    All works(repair,and take items) except animating axe(before optimization)

---
+++ Assets/Spawner.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Timers;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    public GameObject enemy;
+    private float timer = 0;
+    
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (timer >= 10)
+        {
+            Instantiate(enemy, transform.position, transform.rotation);
+            timer = 0;
+        }
+        
+        timer += 1 * Time.deltaTime;
+        
+    }
+}
