30c9eba50a1975d7583ec674b28ee5342b7c8d89
Vladislav Gavrisciuc <angelbeats9632@gmail.com>
2020-02-01 16:15:08 +0200

    Skybox

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -1,43 +1,121 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 using UnityEngine.AI;
+using Random = UnityEngine.Random;
 
 public class Enemy : MonoBehaviour
 {
-
     public GameObject playerToFollow;
-    public NavMeshAgent agent;
-    
+    private NavMeshAgent agent;
+    public GameObject[] positionsToScout;
+    public float attackSpeed;
+    private Animator animator;
+
     private Vector3 selfPosition;
     private Vector3 playerPosition;
-    
+    private float agroRadius;
+    private Player playerScript;
+    private float health = 20;
+
+    public GameObject currentLocationToScout;
+
     // Start is called before the first frame update
     void Start()
     {
+        playerScript = playerToFollow.GetComponent<Player>();
         agent.GetComponent<NavMeshAgent>();
+        animator.GetComponent<Animator>();
+        animator.SetFloat("InputY", -1); // Idle
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawWireSphere(transform.position, agroRadius / 2);
+    }
+
+    private void Scout()
+    {
+        int range = Random.Range(1, 5);
+
+        if (Vector3.Distance(transform.position, currentLocationToScout.transform.position) < 5)
+        {
+            // Debug.Log(Vector3.Distance(transform.position, currentLocationToScout.transform.position));
+            currentLocationToScout = positionsToScout[range];
+            agent.SetDestination(currentLocationToScout.transform.position);
+        }
+        else
+        {
+            agent.SetDestination(currentLocationToScout.transform.position);
+        }
+    }
+
+    private void Move(Vector3 p)
+    {
+        agent.SetDestination(p);
+        animator.SetFloat("InputY", 0); // Run
     }
 
     // Update is called once per frame
     void Update()
     {
+        DoDamage(10);
         selfPosition = transform.position;
         playerPosition = playerToFollow.transform.position;
-        
-        Quaternion lookAtPlayer = Quaternion.LookRotation(playerPosition - selfPosition);
-
-        lookAtPlayer.x = 0;
-        lookAtPlayer.z = 0;
-        
-        transform.rotation = Quaternion.Slerp(transform.rotation, lookAtPlayer, 7F * Time.deltaTime);
-            
         float distance = Vector3.Distance(selfPosition, playerPosition);
 
-        // if (distance >= 3f)
-        // {
-            agent.SetDestination(playerPosition);
-            // transform.Translate(Vector3.forward * 7 * Time.deltaTime);
-        // }
+        if (distance <= 20f)
+        {
+            Quaternion lookAtPlayer = Quaternion.LookRotation(playerPosition - selfPosition);
 
+            lookAtPlayer.x = 0;
+            lookAtPlayer.z = 0;
+
+            transform.rotation = Quaternion.Slerp(transform.rotation, lookAtPlayer, 7F * Time.deltaTime);
+            agent.SetDestination(playerPosition);
+        }
+        else
+        {
+            Scout();
+        }
+        DoDamage(10);
+        if (distance <= 10f)
+        {
+            if (playerToFollow.activeSelf)
+            {
+                if (attackSpeed >= 3)
+                {
+                    DoDamage(10);
+                    attackSpeed = 0;
+                }
+                else
+                {
+                    attackSpeed += 1 * Time.deltaTime;
+                }
+            }
+        }
+    }
+
+    public void takeDamage()
+    {
+        health -= 10;
+        if (health <= 0)
+        {
+            gameObject.SetActive(false);
+        }
+    }
+
+    public void DoDamage(int damage)
+    {
+        Debug.Log("Here");
+        animator.SetFloat("InputY", 1); // Attack
+        playerToFollow.GetComponent<Player>().health -= damage;
+        if (playerToFollow.GetComponent<Player>().health <= 0)
+        {
+            Application.Quit();
+        }
     }
 }
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -6,11 +6,14 @@ public class Player : MonoBehaviour
 {
     public float moveSpeed;
     public float sensitivity;
-    private Ray ray;
+    public float health;
 
     public CharacterController player;
     public GameObject camera;
 
+    public GameObject startPointOfRay;
+
+    private Ray ray;
     private float moveF;
     private float moveB;
 
@@ -20,24 +23,56 @@ public class Player : MonoBehaviour
     void Start()
     {
         player = GetComponent<CharacterController>();
-        
+
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Debug.Log("I pressedMouse");
+
+            RaycastHit rayHit;
+
+            Debug.DrawRay(startPointOfRay.transform.position, startPointOfRay.transform.forward, Color.red, 500f);
+            if (Physics.Raycast(startPointOfRay.transform.position, startPointOfRay.transform.forward, out rayHit, 50))
+            {
+                Debug.Log("i hit " + rayHit.collider.tag);
+                if (rayHit.collider.CompareTag("Enemy"))
+                {
+                    Debug.Log("I hit an enemy");
+                    Enemy component = rayHit.collider.gameObject.GetComponent<Enemy>();
+                    component.takeDamage();
+                }
+            }
+
+        }
+
         moveF = Input.GetAxis("Vertical") * moveSpeed;
         moveB = Input.GetAxis("Horizontal") * moveSpeed;
 
         rotX = Input.GetAxis("Mouse X") + sensitivity;
         rotY = Input.GetAxis("Mouse Y") + sensitivity;
-        
+
         Vector3 movement = new Vector3(moveB, 0, moveF);
-        transform.Rotate(0,rotX,0);
-        camera.transform.Rotate(-rotY,0,0);
+        transform.Rotate(0, rotX, 0);
+        camera.transform.Rotate(-rotY, 0, 0);
+        startPointOfRay.transform.Rotate(-rotY, 0, 0);
 
         movement = transform.rotation * movement;
         player.Move(movement * Time.deltaTime);
-        
+
+    }
+
+
+
+    public void takeDamage()
+    {
+        // health -= 10;
+        // if (health <= 0)
+        // {
+        //     gameObject.SetActive(false);
+        // }
     }
 }
